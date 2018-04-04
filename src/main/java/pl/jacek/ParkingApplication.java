46ccf63e0dd4107be7b31b14e698123afe4745a8
Krystian Zybala <kontakt@kzybala.pl>
2018-04-04 21:26:50 +0200

    Enable JPA

--- src/main/java/pl/jacek/ParkingApplication.java
+++ src/main/java/pl/jacek/ParkingApplication.java
@@ -2,13 +2,14 @@ package pl.jacek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
 public class ParkingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingApplication.class, args);
 	}
-
-
 }
