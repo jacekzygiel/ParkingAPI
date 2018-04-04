7c300e59789f75d7c75116176efa645cc4013b2b
Krystian Zybala <kontakt@kzybala.pl>
2018-04-04 21:25:03 +0200

    Add dto.

---
+++ src/main/java/pl/jacek/parking/location/LocationIdentifier.java
@@ -0,0 +1,14 @@
+package pl.jacek.parking.location;
+
+
+public class LocationIdentifier {
+    private final String id;
+
+    public LocationIdentifier(final String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
