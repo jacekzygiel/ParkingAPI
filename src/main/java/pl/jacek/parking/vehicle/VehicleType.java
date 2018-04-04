a9ed88eb114e9ef96cd72d732cc417f3e3378e7f
Krystian Zybala <kontakt@kzybala.pl>
2018-04-04 21:24:14 +0200

    Refactoring enum

--- src/main/java/pl/jacek/parking/vehicle/VehicleType.java
+++ src/main/java/pl/jacek/parking/vehicle/VehicleType.java
@@ -1,5 +1,21 @@
 package pl.jacek.parking.vehicle;
 
 public enum VehicleType {
-    CAR, MOTORCYCLE, BICYCLE, TRUCK, BUS, DISABLED_CAR
+    CAR(1),
+    MOTORCYCLE(2),
+    BICYCLE(3),
+    TRUCK(4),
+    BUS(5),
+    DISABLED_CAR(6);
+
+    private final int value;
+
+    VehicleType(int value) {
+        this.value = value;
+    }
+
+
+    int getValue() {
+        return value;
+    }
 }
